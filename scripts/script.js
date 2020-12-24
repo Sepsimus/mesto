4cf70a35933b1910f15277baa71ac21819e799c9
Sepsimus <kostya2120@yandex.ru>
2020-12-24 03:11:01 +0300

    Third Push 3.0

--- scripts/script.js
+++ scripts/script.js
@@ -3,28 +3,26 @@ let profileContent = profile.querySelector('.profile__content');
 let profileName = document.querySelector('.profile__name');
 let profileStatus = document.querySelector('.profile__status');
 let profileInfo = profileContent.querySelector('.profile__info');
-let editButton = profileInfo.querySelector('.edit-button')
+let editButton = profileInfo.querySelector('.profile__edit-button')
 let popup = document.querySelector('.popup');
 let popupName = document.querySelector('.popup__input_name');
 let popupStatus = document.querySelector('.popup__input_career');
 let popupWrapper = popup.querySelector('.popup__wrapper');
 let popupExit = popupWrapper.querySelector('.popup__exit');
+// Находим форму в DOM
+let formElement = document.querySelector('.popup__save');// Воспользуйтесь методом querySelector()
+
 
 function openPopup(){
-    popup.classList.remove('popup__none');
+    popup.classList.add('popup_opened');
     popupName.value = profileName.textContent;
     popupStatus.value = profileStatus.textContent;
 }
 
 function closePopup(){
-    popup.classList.add('popup__none');
+    popup.classList.remove('popup_opened');
 }
 
-editButton.addEventListener('click', openPopup);
-popupExit.addEventListener('click', closePopup);
-
-// Находим форму в DOM
-let formElement = document.querySelector('.popup__save');// Воспользуйтесь методом querySelector()
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -38,12 +36,15 @@ function handleFormSubmit (evt) {
     // Воспользуйтесь инструментом .querySelector()
     profileName.textContent = popupName.value;// Получите значение полей из свойства value
     profileStatus.textContent = popupStatus.value;
+    console.log('Форма отправлена');
     // Выберите элементы, куда должны быть вставлены значения полей
 
     // Вставьте новые значения с помощью textContent
     closePopup();
 }
 
+editButton.addEventListener('click', openPopup);
+popupExit.addEventListener('click', closePopup);
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('click', handleFormSubmit);
+formElement.addEventListener('submit', handleFormSubmit);
