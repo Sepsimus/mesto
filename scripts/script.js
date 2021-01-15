ae88cbb514adc643b7a9b51af95e5e3133ef291c
Sepsimus <kostya2120@yandex.ru>
2021-01-15 21:36:45 +0300

    feature/popupImage

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,60 @@
-let profile = document.querySelector('.profile');
-let profileContent = profile.querySelector('.profile__content');
-let profileName = document.querySelector('.profile__name');
-let profileStatus = document.querySelector('.profile__status');
-let profileInfo = profileContent.querySelector('.profile__info');
-let editButton = profileInfo.querySelector('.profile__edit-button')
-let popup = document.querySelector('.popup');
-let popupName = document.querySelector('.popup__input_name');
-let popupStatus = document.querySelector('.popup__input_career');
-let popupWrapper = popup.querySelector('.popup__wrapper');
-let popupExit = popupWrapper.querySelector('.popup__exit');
+const elements = document.querySelector('.elements');
+
+const popupTemplate = document.querySelector('#popup').content;
+
+const profileEdit = popupTemplate.cloneNode(true);
+
+profileEdit.querySelector('.popup__container').name = 'profile-form';
+profileEdit.querySelector('.popup__title').textContent = 'Редактировать профиль';
+profileEdit.querySelectorAll('.popup__input')[0].placeholder = 'Имя';
+profileEdit.querySelectorAll('.popup__input')[0].name = 'profileName';
+profileEdit.querySelectorAll('.popup__input')[1].placeholder = 'О себе';
+profileEdit.querySelectorAll('.popup__input')[1].name = 'career-status';
+profileEdit.querySelectorAll('.popup__input').type = 'text';
+profileEdit.querySelector('.popup__save').textContent = 'Сохранить';
+
+const placeEdit = popupTemplate.cloneNode(true);
+
+placeEdit.querySelector('.popup__container').name = 'place-form';
+placeEdit.querySelector('.popup__title').textContent = 'Новое Место';
+placeEdit.querySelectorAll('.popup__input')[0].placeholder = 'Название';
+placeEdit.querySelectorAll('.popup__input')[0].name = 'placeName';
+placeEdit.querySelectorAll('.popup__input')[1].placeholder = 'Ссылка на картинку';
+placeEdit.querySelectorAll('.popup__input')[1].name = 'link';
+placeEdit.querySelectorAll('.popup__input').type = 'text';
+placeEdit.querySelector('.popup__save').textContent = 'Создать';
+
+elements.append(profileEdit);
+elements.append(placeEdit);
+
+const profile = document.querySelector('.profile');
+const profileName = document.querySelector('.profile__name');
+const profileStatus = document.querySelector('.profile__status');
+const editButton = document.querySelector('.profile__edit-button');
+const addButton = document.querySelector('.profile__add-button');
+
+const popup = document.querySelectorAll('.popup');
+const popupName = document.querySelectorAll('.popup__input')[0];
+const popupStatus = document.querySelectorAll('.popup__input')[1];
+const popupExit = document.querySelectorAll('.popup__exit');
+
 // Находим форму в DOM
-let formElement = document.querySelector('.popup__container');// Воспользуйтесь методом querySelector()
+const formElement = document.querySelector('.popup__container');// Воспользуйтесь методом querySelector()
 
 
-function openPopup(){
-    popup.classList.add('popup_opened');
+function openProfilePopup(){
+    popup[0].classList.add('popup_opened');
     popupName.value = profileName.textContent;
     popupStatus.value = profileStatus.textContent;
 }
 
+function openPlacePopup(){
+    popup[1].classList.add('popup_opened');
+}
+
 function closePopup(){
-    popup.classList.remove('popup_opened');
+    popup[0].classList.remove('popup_opened');
+    popup[1].classList.remove('popup_opened');
 }
 
 
@@ -43,8 +77,12 @@ function handleFormSubmit (evt) {
     closePopup();
 }
 
-editButton.addEventListener('click', openPopup);
-popupExit.addEventListener('click', closePopup);
+editButton.addEventListener('click', openProfilePopup);
+addButton.addEventListener('click', openPlacePopup);
+
+popupExit.forEach(function(item){
+    item.addEventListener('click', closePopup);
+});
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', handleFormSubmit);
